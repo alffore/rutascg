207e4ade295a610856a7bdf2125598a527fd37b3
A. Alfonso F. R <alffore@gmail.com>
2019-12-10 01:00:10 -0600

    Se siguen incluyendo mutaciones

--- main.c
+++ main.c
@@ -21,7 +21,9 @@ dimension = 5;
 int generadorPunto(void);
 int translocacion1a1(int *ori, int *des);
 int corrimiento_D2I(int *ori, int *des);
-
+int corrimiento_I2D(int *ori, int *des);
+int translocacion(int *ori, int *des);
+int inversion3(int *ori, int *des);
 
 
 /**
@@ -65,16 +67,35 @@ int translocacion1a1(int *ori, int *des){
 
     for(i=0;i<dimension;i++){
         if(i==num1){
-            *(des+num2)=*(ori+num1);
+            *(des+num2) = *(ori+num1);
         }else if(i==num2){
-            *(des+num1)=*(ori+num2);
+            *(des+num1) = *(ori+num2);
         }else{
-            *(des+i)=*(ori+i);
+            *(des+i) = *(ori+i);
         }
     }
 
     return 0;
 }
+
+
+int translocacion(int *ori, int *des){
+    int lower =0;
+    int upper=dimension-1;
+    
+    int  m = (rand() %(upper - lower + 1)) + lower;
+
+    for(int i=m;i<dimension;i++){
+        *(des+i-m)=*(ori+i);
+    }
+
+    for(int i=0;i<m;i++){
+        *(des+dimension-1-m+i) = *(ori+i);
+    }
+
+    return 0;
+}
+
 
 /**
  * Funcion 
@@ -86,11 +107,87 @@ int corrimiento_D2I(int *ori, int *des){
 
     int i=0;
 
-
     for(i=1;i<dimension;i++){
-        *(des+i-1)=*(ori+i);
+        *(des+i-1) = *(ori+i);
     }
 
-    *(des+dimension-1)=*(ori);
+    *(des+dimension-1) = *(ori);
+    return 0;
+}
+
+
+int corrimiento_I2D(int *ori, int *des){
+
+    int i=0;
+    int dim=dimension-1;
+
+    for(i=0;i<dim;i++){
+        *(des+i+1) = *(ori+i);
+    }
+
+    *(des)=*(ori+dim);
+    return 0;
+}
+
+
+int inversion3(int *ori, int *des){
+
+    int lower =1;
+    int upper=dimension-2;
+    
+    int  m = (rand() %(upper - lower + 1)) + lower;
+
+    for(int i=0;i<dimension;i++){
+        *(des+i) = *(ori+i);
+    }
+
+    *(des+m-1) = *(ori+m+1);
+    *(des+m+1) = *(ori+m-1);
+
+    return 0;
+}
+
+
+int insercion(int *ori, int *des){
+
+    int lower =0;
+    int upper=dimension-1;
+
+    int c1 = (rand() %(upper - lower + 1)) + lower;
+    int c2 = (rand() %(upper - lower + 1)) + lower;
+
+    int cmin =(c1<=c2)?c1:c2;
+    int cmax =(c1 > c2)?c1:c2;
+
+    int temp[cmax-cmin+1];
+
+    int dimres=dimension-cmax+cmin-1;
+
+    int temp_res[dimres];
+    int k=0;
+
+for(int i=cmin;i<=cmax;i++){
+    temp[i-cmin]=*(ori+i);
+}
+
+for(int j=0;j<dimension;j++){
+    if(j<cmin || j>cmax){
+        temp_res[k]=*(ori+j);
+        k++;
+    }
+}
+
+int c3 = (rand() %(dimres - lower)) + lower;
+
+    for(int i=0;i<dimres;i++){
+        if(i<c3){
+            *(des+i)=temp_res[i];
+        }else if(i==c3{
+
+        }else{
+            *(des+i+cmax-cmin)=temp_res[i];
+        }
+    }
+
     return 0;
 }
