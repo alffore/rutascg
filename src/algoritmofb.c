648854680559f2cc225debfc7267c492f958f6e8
A. Alfonso F. R <alffore@gmail.com>
2019-12-12 22:56:28 -0600

    Update algoritmofb.c

--- src/algoritmofb.c
+++ src/algoritmofb.c
@@ -9,28 +9,51 @@
 
 
 int generaTodos(void);
-unsigned long cantidadPosiblesConv();
-
+unsigned long cantidadPosiblesConv(void);
+void recursivaG(size_t index);
 
 extern unsigned long dimension;
+
+int *aPobFB=NULL;
+int cantidad=0;
 
 /**
  * 
  */
 int generaTodos(void){
 
+    cantidad=cantidadPosiblesConv();
+
+    aPobFB =(int *)malloc(sizeof(int)*cantidad);
+
+    
+
     for(size_t i=0;i<dimension; i++){
-        
+        recursivaG(i);
     }
 
 
+
+    free(aPobFB);
     return 0;
 }
+
+
+void recursivaG(size_t index){
+    for(size_t i=0; i<dimension; i++){
+        for(size_t j=0; j<=index; j++){
+           
+        }
+    }
+    printf("\n");
+
+}
+
 
 /**
  * 
  */ 
-unsigned long cantidadPosiblesConv(){
+unsigned long cantidadPosiblesConv(void){
 
     unsigned long aux=1;
 
